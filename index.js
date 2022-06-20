ed499ea79fb3a857fcaeb4ae8003c7a4aa9e00ee
Sunday A. Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-06-20 19:59:36 +0100

    Simple JavaScript to print into screen

---
+++ index.js
@@ -0,0 +1,10 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+
+let my_name = "Sunday Jimoh"
+let my_height = "5.66m" 
+let my_country = "Nigeria"
+    
+
+alert(my_name);
+alert(my_height);
+alert(my_country);
